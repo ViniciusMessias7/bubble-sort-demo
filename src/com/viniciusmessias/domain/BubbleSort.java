26371a97593f5dd2e476690e2038669cab0dd987
Lamont <viniciusmessias52@gmail.com>
2025-05-25 07:14:08 -0300

    refactor: bubble sort

--- src/com/viniciusmessias/domain/BubbleSort.java
+++ src/com/viniciusmessias/domain/BubbleSort.java
@@ -8,34 +8,32 @@ public class BubbleSort {
     public void addElementInArray(int[] number) {
         for (int i = 0; i < numbers.length; i++) {
             numbers[i] = number[i];
+            ascending[i] = numbers[i];
+            descending[i] = numbers[i];
         }
     }
 
     public int[] ascendingSort() {
-        for (int i = 0; i < numbers.length - 1; i++) {
-            for (int j = 0; j < numbers.length - 1 - i; j++) {
-                if (numbers[j] > numbers[j + 1]) {
-                    int temporary = numbers[j];
-                    numbers[j] = numbers[j + 1];
-                    numbers[j + 1] = temporary;
+        for (int i = 0; i < ascending.length - 1; i++) {
+            for (int j = 0; j < ascending.length - 1 - i; j++) {
+                if (ascending[j] > ascending[j + 1]) {
+                    int temporary = ascending[j];
+                    ascending[j] = ascending[j + 1];
+                    ascending[j + 1] = temporary;
                 }
-                ascending[j] = numbers[j];
-                ascending[j + 1] = numbers[j + 1];
             }
         }
         return ascending;
     }
 
     public int[] descendingSort() {
-        for (int i = 0; i < numbers.length - 1; i++) {
-            for (int j = 0; j < numbers.length - 1 - i; j++) {
-                if (numbers[j] < numbers[j + 1]) {
-                    int temporary = numbers[j];
-                    numbers[j] = numbers[j + 1];
-                    numbers[j + 1] = temporary;
+        for (int i = 0; i < descending.length - 1; i++) {
+            for (int j = 0; j < descending.length - 1 - i; j++) {
+                if (descending[j] < descending[j + 1]) {
+                    int temporary = descending[j];
+                    descending[j] = descending[j + 1];
+                    descending[j + 1] = temporary;
                 }
-                descending[j] = numbers[j];
-                descending[j + 1] = numbers[j + 1];
             }
         }
         return descending;
